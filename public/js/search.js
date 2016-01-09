9e3b4270aa4802f0fa36a42c5b10de2081a290e3
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-09 22:31:13 +0530

    IMPROVED - Added UI handeling for search result

--- public/js/search.js
+++ public/js/search.js
@@ -5,25 +5,45 @@ var Rabto = {
 
 Rabto.ui.init = function() {
 	Rabto.ui.searchBar = document.getElementById('search-bar');
+	Rabto.ui.searchQuery = Rabto.ui.searchBar.getElementsByTagName('input')[0];
+	Rabto.ui.productList = document.getElementById('product-list');
 
 	Rabto.ui.initEvents();
 };
+
+Rabto.ui.renderResults = function(results) {
+	if (results) {
+		results.forEach(function(product) {
+			var card = document.createElement('div');
+			card.className = 'product-card';
+			card.innerHTML = `<div>
+				<div class="product-image"></div>
+			</div>
+			<div class="product-content">
+				<div>${product.id}</div>
+				<div class="product-description">This is basic decription ${product.renter}</div>
+				<div>${product.name}</div>
+			</div>`;
+			Rabto.ui.productList.appendChild(card);
+		});
+	}
+}
 
 Rabto.ui.initEvents = function() {
 	Rabto.ui.searchBar.addEventListener('submit', function(e) {
 		e.preventDefault();
 		Rabto.db.search(
-			Rabto.ui.searchBar.getElementsByTagName('input')[0].value,
+			Rabto.ui.searchQuery.value,
 			function(results, err) {
 				if (err) {
 					console.log('[submit ui.searchBar]', err);
 					return;
 				}
 
-				// TODO: render on the screen
-			})
+				Rabto.ui.renderResults(results);
+			});
 		console.log('[submit initEvents]');
-	})
+	});
 }
 
 Rabto.db.init = function() {
