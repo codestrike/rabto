78d7ff5ed8b44999198b3cc6cc5a991d13ca5abb
azad <azad26195@gmail.com>
2016-01-09 14:00:41 +0000

    IMPOROVED - pg , node-env-file added query function implemented gitignorechanged

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 
 var express = require('express');
 var pg = require('pg');
+var env = require('node-env-file');
 
+
+
+env(__dirname + '/.env');
 // Get port
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.APP_PORT || 8080;
 
+//get postgres configuration
+var credentials = "postgres://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + "/" + process.env.DB_NAME;
 // App
 var app = express();
 
@@ -18,8 +24,28 @@ app.get('/', function (req, res) {
 
 app.get('/api/q/:search', function (req, res) {
 var search_query = req.params.search;
+query('select * from marker', '', function (err, m){
+	(!err)? res.json(m.rows) : res.json({"q":search_query})	
+})
 
 });
+
+var query = function(sql, param, callback) {
+	pg.connect(
+		credentials, 
+		function(err, client, done) {
+			if (err) {
+				console.log(err);
+				callback(err);
+			} else {
+				client.query(sql, param, function(err, result) {
+					done();
+					callback(err, result);
+				});
+			}
+		});
+};
+
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
