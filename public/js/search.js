824706e987e40da57dd4bb5b2b1ca690348e2c5d
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-10 02:30:06 +0530

    IMPROVED - new item creation implemented in frontend - created `db.get()` for frequent `xhr`

--- public/js/search.js
+++ public/js/search.js
@@ -40,7 +40,20 @@ Rabto.ui.initEvents = function() {
 
 	Rabto.ui.fab.addEventListener('click', function(e) {
 		window.location = window.location.origin + '/#openModal';
-	})
+	});
+
+	Rabto.ui.modalCancel.addEventListener('click', function(e) {
+		window.location = window.location.origin + '/#';
+	});
+
+	Rabto.ui.modalSubmit.addEventListener('click', function(e) {
+		var title = Rabto.ui.modalTitle.value;
+		if (!title) return;
+
+		Rabto.db.addItem(title, function(e) {
+			window.location = window.location.origin + '/#';
+		}, true);
+	});
 };
 
 Rabto.ui.init = function() {
@@ -48,25 +61,41 @@ Rabto.ui.init = function() {
 	Rabto.ui.searchQuery = document.getElementById('search-input');
 	Rabto.ui.productList = document.getElementById('product-list');
 	Rabto.ui.fab = document.getElementById('fab-btn');
+	Rabto.ui.modalSubmit = document.getElementById('modal-submit');
+	Rabto.ui.modalCancel = document.getElementById('modal-cancel');
+	Rabto.ui.modalTitle = document.getElementById('modal-title');
 
 	Rabto.ui.initEvents();
 };
 
 // Rabta.db starts
-
-Rabto.db.search = function(query, callback) {
+Rabto.db.get = function(url, callback, noJSON) {
 	var xhr = new XMLHttpRequest();
-	xhr.open('get', window.location.origin + '/api/q/' + encodeURI(query), true);
+	xhr.open('get', url, true);
 	xhr.send();
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			callback(JSON.parse(xhr.responseText));
-		} else if (xhr.readyState == 4 && xhr.status != 200) {
-			callback(null, xhr.status);
+		if (typeof callback === 'function') {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				var data = xhr.responseText;
+				if (!noJSON)
+					data = JSON.parse(data);
+
+				callback(data);
+			} else if (xhr.readyState == 4 && xhr.status != 200) {
+				callback(null, xhr.status);
+			}
 		}
 	}
-}
+};
+
+Rabto.db.search = function(query, callback, noJSON) {
+	Rabto.db.get(window.location.origin + '/api/q/' + encodeURI(query), callback, noJSON);
+};
+
+Rabto.db.addItem = function(title, callback, noJSON) {
+	Rabto.db.get(window.location.origin + '/api/add/item/' + encodeURI(title), callback, noJSON);
+};
 
 Rabto.db.init = function() {}
 
