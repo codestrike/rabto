182eff15d77c8ed24b560cbf2b9e357daebc8947
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-10 01:48:08 +0530

    IMPROVED - modal is opened on click FAB

--- public/js/search.js
+++ public/js/search.js
@@ -3,13 +3,6 @@ var Rabto = {
 	db: {} // Database
 };
 
-Rabto.ui.init = function() {
-	Rabto.ui.searchBar = document.getElementById('search-bar');
-	Rabto.ui.searchQuery = document.getElementById('search-input');
-	Rabto.ui.productList = document.getElementById('product-list');
-
-	Rabto.ui.initEvents();
-};
 
 Rabto.ui.renderResults = function(results) {
 	Rabto.ui.productList.innerHTML = '';
@@ -28,7 +21,7 @@ Rabto.ui.renderResults = function(results) {
 			Rabto.ui.productList.appendChild(card);
 		});
 	}
-}
+};
 
 Rabto.ui.initEvents = function() {
 	Rabto.ui.searchBar.addEventListener('submit', function(e) {
@@ -44,23 +37,38 @@ Rabto.ui.initEvents = function() {
 				Rabto.ui.renderResults(results);
 			});
 	});
-}
 
-Rabto.db.init = function() {
-	Rabto.db.search = function(query, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('get', window.location.origin + '/api/q/' + encodeURI(query), true);
-		xhr.send();
+	Rabto.ui.fab.addEventListener('click', function(e) {
+		window.location = window.location.origin + '/#openModal';
+	})
+};
 
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-				callback(JSON.parse(xhr.responseText));
-			} else if (xhr.readyState == 4 && xhr.status != 200) {
-				callback(null, xhr.status);
-			}
+Rabto.ui.init = function() {
+	Rabto.ui.searchBar = document.getElementById('search-bar');
+	Rabto.ui.searchQuery = document.getElementById('search-input');
+	Rabto.ui.productList = document.getElementById('product-list');
+	Rabto.ui.fab = document.getElementById('fab-btn');
+
+	Rabto.ui.initEvents();
+};
+
+// Rabta.db starts
+
+Rabto.db.search = function(query, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('get', window.location.origin + '/api/q/' + encodeURI(query), true);
+	xhr.send();
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			callback(JSON.parse(xhr.responseText));
+		} else if (xhr.readyState == 4 && xhr.status != 200) {
+			callback(null, xhr.status);
 		}
 	}
 }
+
+Rabto.db.init = function() {}
 
 // Fire In The Hole!
 Rabto.db.init();
