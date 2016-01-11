3e97228cf2b12ea80a05d515f6d2562a82fd2c71
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-11 12:22:34 +0530

    IMPROVED - Introduced `Rabto.lib` for generalized library functions IMPROVED - general method `db.request` is added. `db.get` and `db.post` uses it.

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,7 @@
 var Rabto = {
 	ui: {}, // UI interaction
-	db: {} // Database
+	db: {}, // Database
+	lib: {} // Generalized library
 };
 
 Rabto.ui.sendSMS = function(e) {
@@ -96,10 +97,19 @@ Rabto.ui.init = function() {
 };
 
 // Rabto.db starts
-Rabto.db.get = function(url, callback, noJSON) {
+Rabto.db.request = function(method, url, data, callback, noJSON) {
 	var xhr = new XMLHttpRequest();
-	xhr.open('get', url, true);
-	xhr.send();
+	var body = null;
+
+	if (method == 'get') {
+		url = url + '?' + Rabto.lib.urlSerialize(data);
+	} else {
+		body = Rabto.lib.urlSerialize(data);
+	}
+
+	console.log('[db.request]', method, url, body);
+	xhr.open(method, url, true);
+	xhr.send(body);
 
 	xhr.onreadystatechange = function() {
 		if (typeof callback === 'function') {
@@ -107,14 +117,21 @@ Rabto.db.get = function(url, callback, noJSON) {
 				var data = xhr.responseText;
 				if (!noJSON)
 					data = JSON.parse(data);
-
 				callback(data);
 			} else if (xhr.readyState == 4 && xhr.status != 200) {
 				callback(null, xhr.status);
 			}
 		}
-	}
+	};
+}
+
+Rabto.db.get = function(url, callback, noJSON) {
+	Rabto.db.request('get', url, {}, callback, noJSON);
 };
+
+Rabto.db.post = function(url, data, callback, noJSON) {
+	Rabto.db.request('post', url, data, callback, noJSON);
+}
 
 Rabto.db.sendSMS = function(renter, text) {
 	Rabto.db.get(window.location.origin + '/api/send/sms/' + encodeURI(renter) + '/' + encodeURI(text));
@@ -129,6 +146,14 @@ Rabto.db.addItem = function(title, description, callback, noJSON) {
 };
 
 Rabto.db.init = function() {}
+
+// Generalized library
+Rabto.lib.urlSerialize = function(o) {
+	if (!o) return '';
+	return Object.keys(o).map(function(prop) {
+	  return [prop, o[prop]].map(encodeURIComponent).join("=");
+	}).join("&");
+};
 
 // Fire In The Hole!
 Rabto.db.init();
