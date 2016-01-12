efd0611879d19cb12dcf23af2fb1e703c82db413
Azad Vishwakarma <azad26195@gmail.com>
2016-01-12 16:40:38 +0530

    FIX - db Schema Updated.

--- setup/init_db.sql
+++ setup/init_db.sql
@@ -3,7 +3,7 @@
 -- Drop Table trusts;
 
 -- Table : "items"
-Create Table items(id serial primary key, title varchar(120), renter bigint, description varchar(150), image_url varchar(150));
+Create Table items(id serial primary key, title varchar(120), renter bigint, description varchar(250), image_url varchar(150));
 
 -- Table : "renter"
 Create Table renter(id serial primary key, name varchar(80), email varchar(255), mobile varchar(14), pass varchar(160));
