9be3d9af3201484668c77907eeaf4253e69c18bd
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-14 10:36:44 +0530

    FIX - modal close is based on `<a href="#">` now FIX - db.getUser() result contains `mobile:` property IMPROVED - profile modal open on shell menu click

--- public/js/shell.js
+++ public/js/shell.js
@@ -3,13 +3,24 @@ Rabto.ui.shell = {};
 
 Rabto.ui.shell.init  = function () {
 	var context = Rabto.ui.shell;
+	context.shellMenu = document.getElementById('shell-menu');
 	context.username = document.getElementById('shell-user');
 	context.modalName = document.getElementById('modal-name');
 	context.modalEmail = document.getElementById('modal-email');
 	context.modalMobile = document.getElementById('modal-mobile');
 
 	context.populateUserData();
+	context.initEventListeners();
 };
+
+Rabto.ui.shell.initEventListeners = function() {
+	var context = Rabto.ui.shell;
+	var db = Rabto.db.shell;
+	context.shellMenu.addEventListener('click', function() {
+		context.populateProfileData(db.getUser());
+		window.location = window.location.origin + '/#profileModal';
+	});
+}
 
 Rabto.ui.shell.populateUserData = function() {
 	var context = Rabto.ui.shell;
@@ -34,6 +45,7 @@ Rabto.db.shell.init = function() {};
 Rabto.db.shell.getUser = function() {
 	return JSON.parse(localStorage.getItem('user') || {
 		name: null,
-		email: null
+		email: null,
+		mobile: '0000'
 	});
 };
