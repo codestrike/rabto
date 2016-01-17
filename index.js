79b529486b3a451276ced5dbfdd91bb1bd9b5e87
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-01-17 08:25:36 +0530

    IMPROVEVD - Google based login implemented via passport - new url for login and logout - passposrt no handles session

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var http = require('http').Server(app);
+var https = require('https');
 var env = require('node-env-file');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var cloudy = require('cloudinary');
 
 try {
@@ -19,6 +22,11 @@ var exotel = require('exotel')({
     id   : process.env.EXOTEL_ID, 
     token: process.env.EXOTEL_TOCKEN 
 });
+
+var passportOptions = {
+	host: 'www.google.com',
+	path: '/m8/feeds/contacts/default/full?alt=json&oauth_token='
+};
 
 //Cloudinary Confguration
 	cloudy.config({
@@ -33,6 +41,9 @@ var PORT = process.env.PORT || process.env.APP_PORT || 8080;
 //get postgres configuration
 var credentials = process.env.DATABASE_URL || "postgres://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + "/" + process.env.DB_NAME;
 
+// get Google API credentials
+var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 // Setup static folder
 app.use(express.static('public'));
@@ -45,17 +56,68 @@ app.use(session({
   cookie: { secure: false, maxAge: 360000 }
 }));
 
+// passport store and get user object
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// configure passport for google 
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      passportOptions.path += accessToken;
+      var buff = '';
+      var count = 0;
+      var req = https.get(passportOptions, function(res) {
+        res.on('data', function(d) {
+          buff += d;
+          // console.log('[passport.use accessToken]', count++);
+        });
+        res.on('end', function() {
+          console.log('[passport.use accessToken end]', buff);
+        });
+      });
+      // req.end();
+      req.on('error', function(err) {
+        console.log('[passport.use req.on error]', err);
+      });
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
 // Body Parser
 // app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.get('/', function (req, res) {
-	var sess = req.session;
-	if (sess.user && sess.user.email) {
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', ensureAuthenticated, function(req, res) {
+	// var sess = req.session;
+	// if (sess.user && sess.user.email) {
 		res.sendFile(__dirname + '/index.html');
-	} else {
-		res.sendFile(__dirname + '/login.html');
-	}
+	// } else {
+	// 	res.sendFile(__dirname + '/login.html');
+	// }
+});
+
+app.get('/login', function(req, res) {
+	req.logout();
+	res.sendFile(__dirname + '/login.html');
 });
 
 // session handeling
@@ -100,7 +162,26 @@ app.get('/session/end', function (req, res) {
 		if (err) console.log(err);
 		res.redirect('/?o=i');
 	});
-})
+});
+
+// passport based auth
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.google.com/m8/feeds'] }),
+  function(req, res){
+    // The request will be redirected to Google for authentication, so this
+    // function will not be called.
+});
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 // api for searching
 app.get('/api/q/:search', function (req, res) {
@@ -287,7 +368,12 @@ var serchFilter = function (q,callback){
 
 	}
 
-}
+};
+
+function ensureAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+};
 
 
 http.listen(PORT, function() {
